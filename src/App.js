facb538f3660900d16b61678d9c757eda01acb40
arinmis <mustafa_arinmis@outlook.com>
2022-08-24 08:37:12 +0300

    Create pages with routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer";
 import Header from "./components/header";
+import Home from "./pages/home/home";
+import AboutUs from "./pages/about_us/about_us";
+import ContactUs from "./pages/contact_us/contact_us";
+import Services from "./pages/services/services";
 
 const App = () => {
   return (
     <BrowserRouter>
+
       <div className="min-h-screen flex flex-col">
         <Header />
+        <main>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+        </main>
         <Footer />
       </div>
     </BrowserRouter>
