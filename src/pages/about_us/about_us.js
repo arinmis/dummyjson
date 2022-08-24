cdd1ecab5f5a6814aab10d91994d2248da66004a
Arinmis <mustafa.arinmis@linarit.com>
2022-08-24 15:26:57 +0300

    Put text to about us page

--- src/pages/about_us/about_us.js
+++ src/pages/about_us/about_us.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const AboutUs = () => {
   return (
-    <div>about_us</div>
-  )
-}
+    <div className="w-full flex justify-center main-padding">
+      <p className="card text-left card-w-resp">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+        laborum eius neque molestias accusamus fuga culpa reprehenderit dolores
+        animi vitae, et mollitia eaque iure harum voluptates cum, labore, iusto
+        rem.
+      </p>
+    </div>
+  );
+};
 
 export default AboutUs;
