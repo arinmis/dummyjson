7c5b41f97c6b7a33567a2e103d2595a3d0da638d
Arinmis <mustafa.arinmis@linarit.com>
2022-08-24 08:24:20 +0300

    Add footer js

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark mt-auto text-white text-center">
+      {" "}
+      Copyright &copy; 2022
+    </div>
+  );
+};
+
+export default Footer;
