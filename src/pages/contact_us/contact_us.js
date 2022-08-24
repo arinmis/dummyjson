58e9c9f188f429a0880de93baccb6eca3d8746b3
arinmis <mustafa_arinmis@outlook.com>
2022-08-24 16:20:53 +0300

    Create contact us form

--- src/pages/contact_us/contact_us.js
+++ src/pages/contact_us/contact_us.js
@@ -1,7 +1,34 @@
 import React from "react";
+import Input from "../../components/input";
 
 const ContactUs = () => {
-  return <div>ContactUs</div>;
+  return (
+    <div className="w-full flex  align-center justify-center main-padding">
+      <form className="card text-left card-w-resp grid grid-cols-2 gap-1">
+        <p className="title col-span-2">Say something</p>
+        <Input label={"name"} type={"text"} />
+        <Input label={"title"} type={"text"} />
+        <div className="col-span-2">
+          <Input label={"email"} type={"email"} />
+        </div>
+        <label className="col-span-2 h-full">
+          <span className="text-xs">Message:</span>
+          <textarea className="w-full input-style h-24" />
+        </label>
+        <div className="col-span-2 flex justify-end">
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              alert("We received your message.");
+            }}
+            className="btn-primary mt-3"
+          >
+            Send
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ContactUs;
