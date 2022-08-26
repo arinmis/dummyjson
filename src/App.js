8d522e08b7fa96bf78c94ca42a25b5ad1867d49a
Arinmis <mustafa.arinmis@linarit.com>
2022-08-26 16:59:06 +0300

    Lift product state to partent component

--- src/App.js
+++ src/App.js
@@ -6,15 +6,30 @@ import AboutUs from "./pages/about_us/about_us";
 import ContactUs from "./pages/contact_us/contact_us";
 import Services from "./pages/services/services";
 import ProductDetails from "./pages/product_details/product_details";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [products, setProducts] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    // TODO: handle error
+    axios.get("https://dummyjson.com/products").then((response) => {
+      setProducts(response.data.products);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) return <p>...loading</p>;
   return (
     <BrowserRouter>
       <div className="min-h-screen flex flex-col">
         <Header />
         <main>
           <Routes>
-            <Route index element={<Home />} />
+            <Route index element={<Home products={products} />} />
             <Route path="/about-us" element={<AboutUs />} />
             <Route path="/contact-us" element={<ContactUs />} />
             <Route path="/services" element={<Services />} />
