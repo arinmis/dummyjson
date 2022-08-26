92a023a61abd57944d4ded9f287ae510fd648acf
arinmis <mustafa_arinmis@outlook.com>
2022-08-26 15:00:23 +0300

    List all small products in home

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -5,24 +5,30 @@ import Product from "../../components/product";
 const Home = () => {
   const [products, setProducts] = useState();
   const [isLoading, setIsLoading] = useState(true);
-  const mockProduct = 3;
 
   useEffect(() => {
     setIsLoading(true);
     axios.get("https://dummyjson.com/products").then((response) => {
-      setProducts(response.data);
+      setProducts(response.data.products);
       setIsLoading(false);
     });
   }, []);
   return isLoading ? (
     <p>...loading</p>
   ) : (
-    <div>
-      <Product
-        thumbnail={products.products[mockProduct].thumbnail}
-        price={products.products[mockProduct].price}
-        title={products.products[mockProduct].title}
-      />
+    <div className="flex justify-center">
+
+    <div className="grid  xl:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-3">
+      {products.map((product, index) => {
+        return index === 0 ? null : (
+          <Product
+            thumbnail={product.thumbnail}
+            price={product.price}
+            title={product.title}
+          />
+        );
+      })}
+    </div>
     </div>
   );
 };
