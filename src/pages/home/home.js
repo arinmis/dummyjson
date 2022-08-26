967a48a9882ca33a41cb20f3c93a2c5ff86b9847
arinmis <mustafa_arinmis@outlook.com>
2022-08-26 14:07:58 +0300

    Fetch all products

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    axios.get("https://dummyjson.com/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
   return <div>Home</div>;
 };
 
