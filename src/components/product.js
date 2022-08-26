6779944e37404a84c46356ba4e1b2a6ad8781146
arinmis <mustafa_arinmis@outlook.com>
2022-08-26 16:33:51 +0300

    Make first product greater

--- src/components/product.js
+++ src/components/product.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Product = ({ thumbnail, title, price }) => {
   return (
-    <div className="card  p-2  w-72 h-96 flex flex-col justify-center divide-y">
+    <div className="card p-2  w-72 h-96 flex flex-col justify-center divide-y">
       <div className="h-5/6">
         <img
           src={thumbnail}
-          className="object-cover h-full w-full"
+          className="object-cover h-full w-full rounded-sm"
           alt={`${title} foto`}
         />
       </div>
