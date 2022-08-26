ef88cc05353d3b1bf510099d66714089de44583a
arinmis <mustafa_arinmis@outlook.com>
2022-08-26 14:33:59 +0300

    Create product card

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,15 +1,30 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Product from "../../components/product";
 
 const Home = () => {
   const [products, setProducts] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const mockProduct = 3;
 
   useEffect(() => {
+    setIsLoading(true);
     axios.get("https://dummyjson.com/products").then((response) => {
       setProducts(response.data);
+      setIsLoading(false);
     });
   }, []);
-  return <div>Home</div>;
+  return isLoading ? (
+    <p>...loading</p>
+  ) : (
+    <div>
+      <Product
+        thumbnail={products.products[mockProduct].thumbnail}
+        price={products.products[mockProduct].price}
+        title={products.products[mockProduct].title}
+      />
+    </div>
+  );
 };
 
 export default Home;
