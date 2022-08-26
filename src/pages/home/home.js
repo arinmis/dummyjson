ca14c72ab73d10f143b633461c3aa961c0b6d9d5
Arinmis <mustafa.arinmis@linarit.com>
2022-08-26 16:33:51 +0300

    Make first product greater

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -6,29 +6,55 @@ const Home = () => {
   const [products, setProducts] = useState();
   const [isLoading, setIsLoading] = useState(true);
 
+  const greaterProduct = (productInfo) => {
+    return (
+      <div className="col-span-full h-96 mb-5">
+        <div className="card p-2 h-full grid md:grid-cols-2 grid-cols-1 gap-2">
+          <img
+            src={productInfo.thumbnail}
+            className="object-cover h-full w-full rounded-sm"
+            alt={`${productInfo.title} foto`}
+          />
+          <div className="text-left align-middle">
+            <div className="font-bold"> {productInfo.title}</div>
+            <div>
+              Price:
+              <span className="text-lg">{productInfo.price}</span>
+              <sup>$</sup>
+            </div>
+            <p>Description: {productInfo.description}</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   useEffect(() => {
     setIsLoading(true);
+    // TODO: handle error
     axios.get("https://dummyjson.com/products").then((response) => {
       setProducts(response.data.products);
       setIsLoading(false);
     });
   }, []);
+  
   return isLoading ? (
     <p>...loading</p>
   ) : (
-    <div className="flex justify-center">
-
-    <div className="grid  xl:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-3">
-      {products.map((product, index) => {
-        return index === 0 ? null : (
-          <Product
-            thumbnail={product.thumbnail}
-            price={product.price}
-            title={product.title}
-          />
-        );
-      })}
-    </div>
+    <div className="flex justify-center main-padding">
+      <div className="grid justify-items-center	xl:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-10 divide-y">
+        {/* the first product */}
+        {greaterProduct(products[0])}
+        {products.map((product, index) => {
+          return index === 0 ? null : (
+            <Product
+              thumbnail={product.thumbnail}
+              price={product.price}
+              title={product.title}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
